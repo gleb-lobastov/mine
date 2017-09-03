c3dde127c00ad035852eddba4934ca30baf71299
gleb.lobastov <gleb.lobastov@gmail.com>
2017-09-03 13:26:21 +0500

    1. **Commit will** configure eslint rules. Extends [airbnb](https://github.com/airbnb/javascript) config.    **Why applied** project should be developed under code style convention

---
+++ .eslintrc.js
@@ -0,0 +1,51 @@
+const path = require('path');
+
+module.exports = {
+  extends: 'airbnb',
+  env: {
+    es6: true,
+    browser: true,
+  },
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaVersion: 6,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  plugins: [
+    'react',
+    'jsx-a11y',
+    'import',
+  ],
+  rules: {
+    'import/extensions': [
+      'error',
+      'always',
+      {
+        js: 'never',
+        jsx: 'never',
+      },
+    ],
+    // use app level package.json, not component level
+    'import/no-extraneous-dependencies': [
+      'error',
+      {
+        packageDir: './',
+      },
+    ],
+    // this rule is reasonable, however forces unnecessary rewriting of code
+    'react/prefer-stateless-function': [
+      'off',
+      {},
+    ],
+  },
+  settings: {
+    'import/resolver': {
+      webpack: {
+        config: path.resolve(__dirname, 'webpack.config.js'),
+      },
+    },
+  },
+};
