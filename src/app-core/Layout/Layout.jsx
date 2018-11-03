93bbbce97d951d6ba828a5bb5d86fac43e79dfe7
gleb.lobastov <gleb.lobastov@gmail.com>
2018-11-03 21:51:38 +0300

    feat(font): add Roboto as default font, setup font defaults

--- src/app-core/Layout/Layout.jsx
+++ src/app-core/Layout/Layout.jsx
@@ -1,12 +1,15 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import 'normalize.css';
-import styled from 'styled-components';
-import { Link } from 'react-router-dom';
-import { routes } from 'core/routing';
-import Footer from './Footer';
+import React from "react";
+import PropTypes from "prop-types";
+import "normalize.css";
+import styled from "styled-components";
+import { Helmet } from "react-helmet";
+import { Link } from "react-router-dom";
+import { routes } from "core/routing";
+import Footer from "./Footer";
 
 const StyledGrid = styled.div`
+  font-family: "Roboto", sans-serif;
+  font-size: 16px;
   display: grid;
   grid-template-rows: auto min-content;
   grid-template-columns: 1fr 200px;
@@ -22,39 +25,45 @@ const StyledFooter = styled(Footer)`
 `;
 
 const StyledAside = styled.aside`
- margin-top: 16px;
+  margin-top: 16px;
 `;
 
 class Layout extends React.Component {
   static propTypes = {
-    children: PropTypes.node,
+    children: PropTypes.node
   };
 
   static defaultProps = {
-    children: null,
+    children: null
   };
 
   render() {
-    const {
-      children,
-    } = this.props;
+    const { children } = this.props;
 
     return (
-      <StyledGrid>
-        <StyledMain>
-          {children}
-        </StyledMain>
-        <StyledAside>
-          <Link to={routes.root.toUrl()}> Главная страница </Link>
-          <br />
-          <Link to={routes.blogRoute.toUrl()}> Читать, что я написал </Link>
-          <br />
-          <Link to={routes.travelsRoute.toUrl()}> Смотреть, где я побывал </Link>
-          <br />
-          <Link to={routes.booksRoute.toUrl()}> Узнать, что я прочитал </Link>
-        </StyledAside>
-        <StyledFooter />
-      </StyledGrid>
+      <React.Fragment>
+        <Helmet>
+          <link
+            href="https://fonts.googleapis.com/css?family=Roboto:400,400i,700&amp;subset=cyrillic"
+            rel="stylesheet"
+          />
+        </Helmet>
+        <StyledGrid>
+          <StyledMain>{children}</StyledMain>
+          <StyledAside>
+            <Link to={routes.root.toUrl()}> Главная страница </Link>
+            <br />
+            <Link to={routes.blogRoute.toUrl()}> Читать, что я написал </Link>
+            <br />
+            <Link to={routes.travelsRoute.toUrl()}>
+              Смотреть, где я побывал
+            </Link>
+            <br />
+            <Link to={routes.booksRoute.toUrl()}> Узнать, что я прочитал </Link>
+          </StyledAside>
+          <StyledFooter />
+        </StyledGrid>
+      </React.Fragment>
     );
   }
 }
