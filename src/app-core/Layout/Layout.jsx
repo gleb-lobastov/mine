5329d9dba98595324a171fae2af0d472739e734c
gleb.lobastov <gleb.lobastov@gmail.com>
2018-11-10 20:05:18 +0300

    fix(Layout): setup defaults for mobile screens

--- src/app-core/Layout/Layout.jsx
+++ src/app-core/Layout/Layout.jsx
@@ -38,9 +38,13 @@ class Layout extends React.Component {
     return (
       <React.Fragment>
         <Helmet>
+          <meta
+            name="viewport"
+            content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no"
+          />
           <link
-            href="https://fonts.googleapis.com/css?family=Roboto:400,400i,700&amp;subset=cyrillic"
             rel="stylesheet"
+            href="https://fonts.googleapis.com/css?family=Roboto:300,400,500&amp;subset=cyrillic"
           />
         </Helmet>
         <StyledGrid>
