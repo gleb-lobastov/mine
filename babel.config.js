3250d017e642664ee03f81cbf536a645c5fd2985
gleb.lobastov <gleb.lobastov@gmail.com>
2019-01-03 20:16:19 +0300

    refactor: update dependencies, add jest, update babel 6 -> 7
    Add @babel/plugin-proposal-optional-chaining, as it less verbose and
    less hacky that usage of "get" utility from lodash

---
+++ babel.config.js
@@ -0,0 +1,13 @@
+/* eslint-disable func-names */
+module.exports = function(api) {
+  api.cache(false);
+  return {
+    presets: ['@babel/env', '@babel/react'],
+    plugins: [
+      '@babel/plugin-proposal-class-properties',
+      '@babel/plugin-syntax-dynamic-import',
+      '@babel/plugin-proposal-export-default-from',
+      '@babel/plugin-proposal-optional-chaining',
+    ],
+  };
+};
