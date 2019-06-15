3cc3e1f211586b1f1eba96f737dba2cbcfcf857e
gleb.lobastov <gleb.lobastov@gmail.com>
2019-06-15 17:52:48 +0300

    feat(blog): improve post header rendering
    Markdown has no option to render date in less cathy manner.

--- src/packages/literature/pages/Blog/blocks/Post.jsx
+++ src/packages/literature/pages/Blog/blocks/Post.jsx
@@ -1,11 +1,23 @@
+
 import React from 'react';
 import PropTypes from 'prop-types';
 import ReactMarkdown from 'react-markdown';
 import format from 'date-fns/format';
 import ru from 'date-fns/locale/ru';
+import { withStyles } from '@material-ui/core/styles';
+
+const styles = {
+  date: {
+    color: "#999",
+    fontSize: "75%",
+    fontWeight: "normal",
+    marginLeft: "4px"
+  },
+};
 
 class Post extends React.PureComponent {
   static propTypes = {
+    classes: PropTypes.objectOf(PropTypes.string).isRequired,
     source: PropTypes.shape({
       date: PropTypes.string.isRequired,
       content: PropTypes.string.isRequired,
@@ -13,23 +25,24 @@ class Post extends React.PureComponent {
     }).isRequired,
   };
 
-  assembleContent() {
+  render() {
     const {
-      source: { content, date, header },
+      classes,
+      source: { date, header, content },
     } = this.props;
 
-    return `### ${header} (*${format(date, 'eeeeee, d MMMM yyyy', {
-      locale: ru,
-    })}*)\n\n${content}`;
-  }
-
-  render() {
     return (
       <article>
-        <ReactMarkdown source={this.assembleContent()} />
+        <h3>
+          {header}
+          <span className={classes.date}>
+            {format(date, 'eeeeee, d MMMM yyyy', { locale: ru })}
+          </span>
+        </h3>
+        <ReactMarkdown source={content} />
       </article>
     );
   }
 }
 
-export default Post;
+export default withStyles(styles)(Post);
