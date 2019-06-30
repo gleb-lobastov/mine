26ebad0c6ca3dc87b64a344f54b5c0188f71591d
gleb.lobastov <gleb.lobastov@gmail.com>
2019-06-30 19:31:02 +0300

    fix(travel): fix order resolution for Add Visit button
    There is exist bugs with ordering, need to return to them as soon
    as possible

--- src/modules/utilities/algorithms/createOrderCalculator.js
+++ src/modules/utilities/algorithms/createOrderCalculator.js
@@ -9,8 +9,8 @@ export default ({ orderResolver: resolveOrder }) => ({ index, collection }) => {
   if (index >= collection.length - 1) {
     return resolveOrder(collection[collection.length - 1]) + 1;
   }
-  const prevOrder = resolveOrder(collection[index]);
-  const nextOrder = resolveOrder(collection[index + 1]);
+  const prevOrder = resolveOrder(collection[index - 1]);
+  const nextOrder = resolveOrder(collection[index]);
   const randomness = ((Math.random() - 0.5) * (nextOrder - prevOrder)) / 2;
   return (prevOrder + nextOrder) / 2 + randomness;
 };
