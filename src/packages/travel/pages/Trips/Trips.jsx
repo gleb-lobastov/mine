2a9c2c6b75c2e506840fd33a3000b85d7797eb70
gleb.lobastov <gleb.lobastov@gmail.com>
2019-06-30 15:05:35 +0300

    fix(travel): additional params could be undefined (if only order update)

--- src/packages/travel/pages/Trips/Trips.jsx
+++ src/packages/travel/pages/Trips/Trips.jsx
@@ -74,7 +74,7 @@ const Trips = ({
   );
 
   const handleVisitUpdate = useCallback(
-    (visit, { indexInCollection, collection, tripId }) =>
+    (visit, { indexInCollection, collection, tripId } = {}) =>
       request(
         submitVisit({
           visit,
