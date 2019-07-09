78d60751db03bc986f73f089b5a9568449d7c6d8
gleb.lobastov <gleb.lobastov@gmail.com>
2019-07-09 23:25:01 +0300

    ci: plug redux devtools extension

--- src/core/store/createStore.js
+++ src/core/store/createStore.js
@@ -1,16 +1,22 @@
 /* global window __IS_DEV_MODE__  */
-import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import { requestReducer, requestMiddleware } from 'core/connection';
 
 const reducer = combineReducers({
   requestKit: requestReducer,
 });
 
+const composeEnhancers =
+  (typeof window !== 'undefined' &&
+    // eslint-disable-next-line no-underscore-dangle
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
+  compose;
+
 export default initialState => {
   const store = createStore(
     reducer,
     initialState,
-    applyMiddleware(requestMiddleware),
+    composeEnhancers(applyMiddleware(requestMiddleware)),
   );
 
   if (__IS_DEV_MODE__) {
