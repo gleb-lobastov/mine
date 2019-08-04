2dbeb28227a71ebbb91aee13983b03a1a511462b
gleb.lobastov <gleb.lobastov@gmail.com>
2019-08-04 17:15:16 +0300

    feat(travel): support lat and lon fields in location model

--- src/packages/travel/models/locations/adapters.js
+++ src/packages/travel/models/locations/adapters.js
@@ -2,10 +2,14 @@ export const toClient = ({
   country_name: rawCountryName,
   id: rawLocationId,
   location_name: rawLocationName,
+  lat: rawLatitude,
+  lon: rawLongitude,
 }) => ({
   countryName: rawCountryName,
   locationId: rawLocationId,
   locationName: rawLocationName,
+  lat: rawLatitude,
+  lon: rawLongitude,
 });
 
 export const toServer = () => undefined;
