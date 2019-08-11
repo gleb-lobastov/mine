75e8e441f83634febf7bbf026da741ce518ebfdd
gleb.lobastov <gleb.lobastov@gmail.com>
2019-08-11 14:17:56 +0500

    feat: support countryId field in location model

--- src/packages/travel/models/locations/adapters.js
+++ src/packages/travel/models/locations/adapters.js
@@ -1,10 +1,12 @@
 export const toClient = ({
+  country_id: rawCountryId,
   country_name: rawCountryName,
   id: rawLocationId,
   location_name: rawLocationName,
   lat: rawLatitude,
   lon: rawLongitude,
 }) => ({
+  countryId: rawCountryId,
   countryName: rawCountryName,
   locationId: rawLocationId,
   locationName: rawLocationName,
