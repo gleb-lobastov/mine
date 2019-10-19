91257f5bcc449b3b5be21c46cf452e4dc56fa452
gleb.lobastov <gleb.lobastov@gmail.com>
2019-10-19 12:54:08 +0300

    feat(travel): add livejournal link to travel dashboard

--- src/packages/travel/pages/Dashboard/TravelDashboard.jsx
+++ src/packages/travel/pages/Dashboard/TravelDashboard.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Markdown from 'modules/components/Markdown';
+import Button from '@material-ui/core/Button';
 import { withPaths } from 'core/context/AppContext';
 
 export default withPaths(({ namedPaths }) => (
@@ -14,6 +15,16 @@ export default withPaths(({ namedPaths }) => (
 а раз так, то и на инфографику забил.
   `}
     />
+    <div>
+      <Button
+        component="a"
+        target="_blank"
+        rel="nofollow noopener"
+        href="https://lobastov.livejournal.com/tag/%D0%9F%D1%83%D1%82%D0%B5%D1%88%D0%B5%D1%81%D1%82%D0%B2%D0%B8%D0%B5"
+      >
+        Про путешествия в ЖЖ
+      </Button>
+    </div>
     <a href="https://my.flightradar24.com/lobastov">
       <img
         src="https://banners-my.flightradar24.com/lobastov.png"
