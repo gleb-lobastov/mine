27a3497a11b828ad07fc47d3611b2b8a78277f7e
gleb.lobastov <gleb.lobastov@gmail.com>
2019-10-20 00:31:38 +0300

    feat(code): add job description + noreferrer

--- src/packages/code/pages/CodeDashboard/CodeDashboard.jsx
+++ src/packages/code/pages/CodeDashboard/CodeDashboard.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Typography from '@material-ui/core/Typography';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
@@ -18,33 +19,49 @@ class CodeDashboard extends React.PureComponent {
       },
     } = this.props;
     return (
-      <List>
-        <ListItem
-          button={true}
-          component="a"
-          target="_blank"
-          rel="nofollow noopener"
-          href="https://github.com/gleb-lobastov"
-        >
-          <ListItemText primary="Гитхаб" />
-        </ListItem>
-        <ListItem
-          button={true}
-          component="a"
-          target="_blank"
-          rel="nofollow noopener"
-          href="https://www.linkedin.com/in/glebin"
-        >
-          <ListItemText primary="Linkedin" />
-        </ListItem>
-        <ListItem
-          button={true}
-          component={Link}
-          to={articlesRoute.toUrl({ slug: 'importThis' })}
-        >
-          <ListItemText primary="Статья &laquo;19 принципов достижения дзена при написании компьютерных программ&raquo;" />
-        </ListItem>
-      </List>
+      <>
+        <Typography variant="h4">
+          Работаю техлидом в{' '}
+          <a
+            href="https://www.onetwotrip.com"
+            target="_blank"
+            rel="nofollow noreferrer noopener"
+          >
+            OneTwoTrip
+          </a>
+        </Typography>
+        <Typography variant="subtitle2" color="textSecondary">
+          JavaScript, ES6+, React, Redux, Restful API, Jest, Git, HTML5, CSS3,
+          Sass, Npm, Webpack, Babel, ESLint, Prettier
+        </Typography>
+        <List>
+          <ListItem
+            button={true}
+            component="a"
+            target="_blank"
+            rel="nofollow noreferrer noopener"
+            href="https://github.com/gleb-lobastov"
+          >
+            <ListItemText primary="Гитхаб" />
+          </ListItem>
+          <ListItem
+            button={true}
+            component="a"
+            target="_blank"
+            rel="nofollow noreferrer noopener"
+            href="https://www.linkedin.com/in/glebin"
+          >
+            <ListItemText primary="Linkedin" />
+          </ListItem>
+          <ListItem
+            button={true}
+            component={Link}
+            to={articlesRoute.toUrl({ slug: 'importThis' })}
+          >
+            <ListItemText primary="Статья &laquo;19 принципов достижения дзена при написании компьютерных программ&raquo;" />
+          </ListItem>
+        </List>
+      </>
     );
   }
 }
