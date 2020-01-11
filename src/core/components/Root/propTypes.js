e7f259fa92c45322b8e0cefae04f4ec1c465d524
gleb.lobastov <gleb.lobastov@gmail.com>
2020-01-11 03:16:30 +0300

    docs: fix prop types

--- src/core/components/Root/propTypes.js
+++ src/core/components/Root/propTypes.js
@@ -1,28 +1,9 @@
 import PropTypes from 'prop-types';
-import reactComponentPropType from 'modules/customPropTypes/reactComponentPropType';
-
-export const routeConfigPropTypes = {
-  path: PropTypes.string,
-  Component: PropTypes.oneOfType([PropTypes.func, PropTypes.object]),
-};
-
-export const routesMenuConfigType = {
-  caption: PropTypes.string,
-};
 
 export const packagePropTypes = {
-  id: PropTypes.string,
-  packageName: PropTypes.string,
-  title: PropTypes.shape({
-    caption: PropTypes.string,
-    icon: reactComponentPropType,
-  }),
-  routing: PropTypes.shape({
-    routes: PropTypes.arrayOf(PropTypes.shape(routeConfigPropTypes)),
-    menu: PropTypes.arrayOf(PropTypes.shape(routesMenuConfigType)),
-  }),
+  mountPath: PropTypes.string,
 };
 
 export const configPropTypes = {
-  packages: PropTypes.arrayOf(PropTypes.shape(packagePropTypes)).isRequired,
+  packages: PropTypes.objectOf(PropTypes.shape(packagePropTypes)).isRequired,
 };
