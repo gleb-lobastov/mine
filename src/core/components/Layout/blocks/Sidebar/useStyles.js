0d73d439edfb2dcfc7fe19f8d83ed2f8e40cdb91
gleb.lobastov <gleb.lobastov@gmail.com>
2020-02-02 14:43:05 +0300

    fix: sidebar styles usage
    Previously styles was ignored

--- src/core/components/Layout/blocks/Sidebar/useStyles.js
+++ src/core/components/Layout/blocks/Sidebar/useStyles.js
@@ -2,16 +2,14 @@ import { makeStyles } from '@material-ui/core/styles';
 
 const drawerWidth = 240;
 
-export default function() {
-  return makeStyles(theme => ({
-    sidebar: {
-      [theme.breakpoints.up('sm')]: {
-        width: drawerWidth,
-        flexShrink: 0,
-      },
-    },
-    drawerPaper: {
+export default makeStyles(theme => ({
+  sidebar: {
+    [theme.breakpoints.up('sm')]: {
       width: drawerWidth,
+      flexShrink: 0,
     },
-  }));
-}
+  },
+  drawerPaper: {
+    width: drawerWidth,
+  },
+}));
