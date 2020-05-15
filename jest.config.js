18ad13ec8174767df00a9f8fffc02a3358bbc487
gleb.lobastov <gleb.lobastov@gmail.com>
2020-05-16 01:21:38 +0300

    fix: jest can't find babel.config.js
    https://github.com/facebook/jest/issues/8006

--- jest.config.js
+++ jest.config.js
@@ -4,4 +4,7 @@ module.exports = {
     '^(modules|core|configuration|packages)/(.*)$': '<rootDir>/src/$1/$2',
     '^(main|travel|literature|code|auth)/(.*)$': '<rootDir>/src/packages/$1/$2',
   },
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { rootMode: 'upward' }],
+  },
 };
