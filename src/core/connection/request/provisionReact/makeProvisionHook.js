32522b110f2aa527df0abd30e180860985568548
gleb.lobastov <gleb.lobastov@gmail.com>
2020-05-16 03:42:50 +0300

    fix: prevent refetch data for same domain if nothing triggers refetch
    Request should be send for second time only if requirements observed
    value is changed, or provision is marked as invalid. See comment in
    code for details
    
    More react-way solution may be to track state in global context, but it
    looks like overkill, as require additional actions from lib user, to setup
    that context, smudge the code and increase its amount

--- src/core/connection/request/provisionReact/makeProvisionHook.js
+++ src/core/connection/request/provisionReact/makeProvisionHook.js
@@ -1,6 +1,28 @@
-import { useEffect, useMemo, useRef } from 'react';
+import { useEffect, useMemo } from 'react';
 import debounce from 'lodash/debounce';
 import { checkIsRequirementsChanged } from '../utils';
+
+// Global storage is used to preserve requirements from previous render even
+// between unmount/mount cycle or component. Moreover even different components
+// could reference same domain and keep provision state in sync.
+// Previous values is only used to detect changes that could trigger provision
+// refetch (if observed is change, or provision is marked as invalid).
+// Old values not used and should not be used for any other reasons.
+const memoryState = (() => {
+  const state = {};
+  return {
+    get: requirements => {
+      const domain = requirements?.domain;
+      return domain ? state[domain] : undefined;
+    },
+    set: nextState => {
+      const domain = nextState?.requirements?.domain;
+      if (domain) {
+        state[domain] = nextState;
+      }
+    },
+  };
+})();
 
 export default function makeProvisionHook({
   requestHandler,
@@ -23,13 +45,12 @@ export default function makeProvisionHook({
       [debounceRequest],
     );
 
-    const previousRef = useRef({});
     useEffect(
       () => {
         const {
           requirements: prevRequirements,
           provision: prevProvision,
-        } = previousRef.current;
+        } = memoryState.get(requirements);
 
         const comparisonResult = checkIsRequirementsChanged(
           { requirements: prevRequirements, provision: prevProvision },
@@ -43,7 +64,7 @@ export default function makeProvisionHook({
           });
         }
 
-        previousRef.current = { requirements, provision };
+        memoryState.set({ requirements, provision });
       },
       [requirements, provision],
     );
