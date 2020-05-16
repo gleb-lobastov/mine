60f6db4b51b5ef9866a41c6910fdfd2b474673a7
gleb.lobastov <gleb.lobastov@gmail.com>
2020-05-16 03:10:11 +0300

    fix: use commonjs modules for test env
    To play nice with jest

--- babel.config.js
+++ babel.config.js
@@ -1,8 +1,10 @@
 /* eslint-disable func-names */
 module.exports = function(api) {
-  api.cache(false);
   return {
-    presets: ['@babel/env', '@babel/react'],
+    presets: [
+      ['@babel/env', { modules: api.env('test') ? 'commonjs' : false }],
+      '@babel/react',
+    ],
     plugins: [
       '@babel/plugin-proposal-class-properties',
       '@babel/plugin-syntax-dynamic-import',
