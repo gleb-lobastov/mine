873f73c59e4f6f3c36f9787bbabb6446a7cb1ab9
gleb.lobastov <gleb.lobastov@gmail.com>
2020-07-15 01:29:55 +0300

    test: fix testMatch pattern
    Otherwise trying to run all files inside __tests__ dir

--- jest.config.js
+++ jest.config.js
@@ -4,6 +4,7 @@ module.exports = {
     '^(modules|core|configuration|packages)/(.*)$': '<rootDir>/src/$1/$2',
     '^(main|travel|literature|code|auth)/(.*)$': '<rootDir>/src/packages/$1/$2',
   },
+  testMatch: ['**/__tests__/**/test-*.[jt]s?(x)'],
   transform: {
     '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { rootMode: 'upward' }],
   },
