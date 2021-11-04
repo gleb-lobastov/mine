a474a79cef938dfc31012251d62ee841fdc465e5
gleb.lobastov <gleb.lobastov@gmail.com>
2021-11-05 02:28:08 +0300

    fix: only one visit page
    Feature Toggles not implemented yey

--- src/packages/travel/pages/VisitsPage/VisitsPage.jsx
+++ src/packages/travel/pages/VisitsPage/VisitsPage.jsx
@@ -1,15 +1,6 @@
 import React from 'react';
-import { FeatureToggle } from 'modules/components/FeatureToggles';
 import RefactoredVisitsPage from '../RefactoredVisitsPage';
-import DeprecatedVisitsPage from './DeprecatedVisitsPage';
 
 export default function VisitsPage(props) {
-  return (
-    <FeatureToggle name="visitsPageRefactor">
-      <RefactoredVisitsPage {...props} />
-      <FeatureToggle.Fallback>
-        <DeprecatedVisitsPage {...props} />
-      </FeatureToggle.Fallback>
-    </FeatureToggle>
-  );
+  return <RefactoredVisitsPage {...props} />;
 }
