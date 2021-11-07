a0eb019040be2d35b6bd6863aa25619e821c89c8
gleb.lobastov <gleb.lobastov@gmail.com>
2021-11-07 22:34:37 +0300

    fix: dont fail if response is null

--- src/core/connection/request/intgReduxModelNormalized/strategyEnhancer/index.js
+++ src/core/connection/request/intgReduxModelNormalized/strategyEnhancer/index.js
@@ -2,7 +2,8 @@ import { createEntitiesAction, createEntitiesErrorAction } from '../state';
 
 export default next => (requirements, dispatch, getState) =>
   next(requirements, dispatch, getState).then(
-    ({ result, entities }) => {
+    response => {
+      const { result, entities } = response ?? {};
       if (entities) {
         dispatch(createEntitiesAction(entities, { key: 'key', requirements }));
       }
